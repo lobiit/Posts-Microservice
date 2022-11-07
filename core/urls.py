e4e88409893518328902b7686411402910412a72
Tumbo Lobi John <johnlobiit@gmail.com>
2022-11-07 17:21:44 +0300

    created get and post API view

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .views import PostAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts', PostAPIView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core.models import Post
+from core.serializers import PostSerializer
+
 
 # Create your views here.
+class PostAPIView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
